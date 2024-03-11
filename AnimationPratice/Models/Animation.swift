99f5ac1418dbe039fbc26496a5d3b35a640dcbf7
Denis Denisov <denis.denisov8701@gmail.com>
2024-03-11 22:24:26 +0100

    Prepare Project for beginning

---
+++ AnimationPratice/Models/Animation.swift
@@ -0,0 +1,8 @@
+//
+//  Animation.swift
+//  AnimationPratice
+//
+//  Created by Denis Denisov on 11/3/24.
+//
+
+import Foundation
---
+++ AnimationPratice/Services/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  AnimationPratice
+//
+//  Created by Denis Denisov on 11/3/24.
+//
+
+import Foundation
